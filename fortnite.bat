5dbc02a89b26efe951428b34c1b387be2253b498
DevArif2 <102538661+DevArif2@users.noreply.github.com>
2022-09-03 23:16:24 +0200

    CRACK

---
+++ fortnite.bat
@@ -0,0 +1,8 @@
+@echo off
+
+echo Starting Fortnite...
+start https://cdn2.unrealengine.com/21br-keyart-egs-launcher-pdp-2560x1440-2560x1440-75b944294100.jpg
+timeout /t 5 /nobreak
+start https://i.ytimg.com/vi/VR9sDaKdfgs/maxresdefault.jpg
+timeout /t 3 /nobreak
+start https://www.youtube.com/watch?v=vzhZVhZLtZA?t=200
